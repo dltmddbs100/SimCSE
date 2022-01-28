44010a7e7a0556a08b2253cb16626e4fa1e35ea4
Seung Yoon Lee <55730591+dltmddbs100@users.noreply.github.com>
2022-01-28 19:10:30 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -3,6 +3,7 @@ import time
 import os
 
 import torch
+import torch.optim as optim
 from torch import nn
 from torch.cuda import amp
 
@@ -10,20 +11,27 @@ from data.dataloader import get_loader
 from model.model import SimCSE
 
 def Trainer(args, data_loader, model, loss, metric):
-  
-  print('\n======== Training ========')
+
+  no_decay = ['bias', 'LayerNorm.weight']
+  optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+    'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+    'weight_decay': 0.0}
+  ]
+
   scaler = amp.GradScaler()
-  optimizer = args['optimizer']
-  criterion = args['criterion']
+  optimizer = optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+  criterion = nn.CrossEntropyLoss()
   
-  for epoch_i in range(0, args['max_epochs']):
+  for epoch_i in range(0, args.max_epochs):
     
     # ========================================
     #               Training
     # ========================================
 
     print("")
-    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, args['max_epochs']))
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, args.max_epochs))
     model.train()
     t0 = time.time()
     total_train_loss = 0
@@ -78,14 +86,12 @@ def Trainer(args, data_loader, model, loss, metric):
       print(f"\rValidation Batch {i+1}/{total_val_batch}", end='')
     score=metric.cal_dev_score(i, score_indicator)
     
-    model.model.save_pretrained(os.path.join(args['weight_path'], f"{args['model_name'].replace('/','_')}_epochs:{epoch_i+1}_cosim:{round(score_indicator['eval_spearman_cosine'],4)}"))
+    model.model.save_pretrained(os.path.join(args.weight_path, f"{args.model_name.replace('/','_')}_epochs:{epoch_i+1}_cosim:{round(score_indicator['eval_spearman_cosine'],4)}"))
 
 
-def Tester(args, loss, metric):
+def Tester(args, data_loader, model, loss, metric):
 
-  data_loader=get_loader(args,'test')
-
-  model=SimCSE(args, mode='test').to(args['device'])
+  model.to(args.device)
   model.eval()
 
   total_test_loss=0
@@ -99,8 +105,7 @@ def Tester(args, loss, metric):
                      'eval_spearman_euclidean': 0,
                      'eval_pearson_dot': 0,
                      'eval_spearman_dot': 0}
-  
-  print('\n======== Inference ========')
+
   for i, batch in enumerate(data_loader['test']):
     inputs = batch
 
